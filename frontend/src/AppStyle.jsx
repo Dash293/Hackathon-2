6fe85d141cd65118b00c9da431361f9ec3c8e308
n-faycel <faycel.nasmi@outlook.fr>
2022-06-29 23:48:54 +0200

    creation variable css

--- frontend/src/AppStyle.jsx
+++ frontend/src/AppStyle.jsx
@@ -1,3 +1,14 @@
 import styled from "styled-components";
 
-export default styled.div``;
+export default styled.div`
+  @import url("https://fonts.googleapis.com/css2?family=Hind:wght@300&family=Montserrat:ital@1&display=swap");
+  //VARIABLES DEFINITION
+  --colorBlueNavBar: #202a38;
+  --colorLightBlue: #384152;
+  --colorBackgroundLightGrey: #dadfe3;
+  --colorBackgroundOrangeCard: #ecb78d;
+  --colorTextBlack: #000000;
+  --colorTextWhite: #ffffff;
+  --font-familyTitle: "Montserrat", bold;
+  --font-familyBody: "Hind", sans-serif;
+`;
