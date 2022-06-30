2bb3d71552d8c19e9a79ab560d7325cbad6b5a1f
Valentin Coppin <valentincoppin@hotmail.fr>
2022-06-30 15:02:43 +0200

    working on login page

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,16 +1,13 @@
-import Navbar from "@components/Navbar";
-import Login from "@pages/Home";
+import Login from "@pages/Login";
+
 import { Routes, Route } from "react-router-dom";
 
 function App() {
   return (
     <div className="App">
-      <Navbar />
-      <main>
-        <Routes>
-          <Route path="/" element={<Login />} />
-        </Routes>
-      </main>
+      <Routes>
+        <Route path="/" element={<Login />} />
+      </Routes>
     </div>
   );
 }
