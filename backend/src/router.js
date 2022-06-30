4480dffdb81ddfa557083cc386f4ad07c695a001
Amar <amar@uub.localdomain>
2022-06-30 15:18:40 +0200

    adding main routes

--- backend/src/router.js
+++ backend/src/router.js
@@ -1,6 +1,11 @@
 const express = require("express");
 
-const { ItemController } = require("./controllers");
+const {
+  ItemController,
+  EmployeeController,
+  IdeaController,
+  ProjectController,
+} = require("./controllers");
 
 const router = express.Router();
 
@@ -9,5 +14,8 @@ router.get("/items/:id", ItemController.read);
 router.put("/items/:id", ItemController.edit);
 router.post("/items", ItemController.add);
 router.delete("/items/:id", ItemController.delete);
+router.get("/collaborateur", EmployeeController.browse);
+router.get("/projet", IdeaController.browse);
+router.get("/boîte-à-idées", ProjectController.browse);
 
 module.exports = router;
