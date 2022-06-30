1a02c0ee618096712ee782bf265b5669f1b55c75
Valentin Coppin <valentincoppin@hotmail.fr>
2022-06-30 11:02:33 +0200

    initilalisation with globalstyle and routes

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -2,15 +2,17 @@ import React from "react";
 import ReactDOM from "react-dom";
 import { Reset } from "styled-reset";
 import { BrowserRouter } from "react-router-dom";
-
+import GlobalStyle from "./GlobalStyle";
 import App from "./App";
 
-ReactDOM.render(
+const root = ReactDOM.createRoot(document.getElementById("root"));
+
+root.render(
   <React.StrictMode>
-    <Reset />
     <BrowserRouter>
+      <Reset />
+      <GlobalStyle />
       <App />
     </BrowserRouter>
-  </React.StrictMode>,
-  document.getElementById("root")
+  </React.StrictMode>
 );
