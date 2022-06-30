a7996961a296d796cc6d832130289a01c269cd4c
Gonaline <aline-gonce_student2022@wilder.school>
2022-06-30 09:55:25 +0200

    add pictures & create cardProject component

---
+++ frontend/src/components/CardProject/index.jsx
@@ -0,0 +1,12 @@
+import SCardProject from "@components/CardProject/style"
+
+export default function CardProject() {
+
+
+  return (
+    <SCardProject>
+<div>cardproject</div>
+
+    </SCardProject>
+  );
+}
