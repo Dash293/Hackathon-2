2faf8f0e75a0104a41d1ad45b30029e28c79e2f4
n-faycel <faycel.nasmi@outlook.fr>
2022-06-30 11:27:15 +0200

    init fix

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -5,10 +5,12 @@ import { Routes, Route } from "react-router-dom";
 function App() {
   return (
     <div className="App">
-      <Routes>
-        <Route path="/" element={<Login />} />
-      </Routes>
       <Navbar />
+      <main>
+        <Routes>
+          <Route path="/" element={<Login />} />
+        </Routes>
+      </main>
     </div>
   );
 }
--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -5,14 +5,13 @@ import { BrowserRouter } from "react-router-dom";
 import GlobalStyle from "./GlobalStyle";
 import App from "./App";
 
-const root = ReactDOM.createRoot(document.getElementById("root"));
-
-root.render(
+ReactDOM.render(
   <React.StrictMode>
+    <Reset />
+    <GlobalStyle />
     <BrowserRouter>
-      <Reset />
-      <GlobalStyle />
       <App />
     </BrowserRouter>
-  </React.StrictMode>
+  </React.StrictMode>,
+  document.getElementById("root")
 );
