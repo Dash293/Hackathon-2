cd48e45df999eb4cfd7ed74d66babb4318428a46
Gonaline <aline-gonce_student2022@wilder.school>
2022-06-30 21:16:51 +0200

    add pictures

--- frontend/src/components/CardProject/index.jsx
+++ frontend/src/components/CardProject/index.jsx
@@ -1,9 +1,8 @@
-import { Link } from "react-router-dom";
+// import { Link } from "react-router-dom";
 import PropTypes from "prop-types";
 import SCardProject from "@components/CardProject/style";
 
 export default function CardProject({
-  key,
   description,
   client,
   launchDate,
@@ -12,19 +11,16 @@ export default function CardProject({
 }) {
   return (
     <SCardProject>
-      <Link to={`/projets/${key}`}>
-        <p>Démarré le {launchDate}</p>
-        <p>{description}</p>
-        <p>Client : {client}</p>
-        <img src={`../../src/assets/${logo}`} alt="logo" />
-        <p>Agence de {mainAgency}</p>
-      </Link>
+      <p>Démarré le {launchDate}</p>
+      <p>{description}</p>
+      <p>Client : {client}</p>
+      <img src={`../../src/assets/${logo}`} alt="logo" />
+      <p>Agence de {mainAgency}</p>
     </SCardProject>
   );
 }
 
 CardProject.propTypes = {
-  key: PropTypes.number.isRequired,
   description: PropTypes.string.isRequired,
   client: PropTypes.string.isRequired,
   launchDate: PropTypes.string.isRequired,
