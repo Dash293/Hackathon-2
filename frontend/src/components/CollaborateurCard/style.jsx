0a2715bd4e9ddb52648393fcbcfc6ce6a4b344de
Gonaline <aline-gonce_student2022@wilder.school>
2022-07-01 11:12:00 +0200

    mobile version style

--- frontend/src/components/CollaborateurCard/style.jsx
+++ frontend/src/components/CollaborateurCard/style.jsx
@@ -28,4 +28,34 @@ export default styled.div`
     width: auto;
     margin-bottom: 5vh;
   }
+  @media screen and (max-width: 700px) {
+    /* height: 100vh; */
+    width: 100vw;
+    background: var(--colorBackgroundOrangeCard);
+    border-radius: 0;
+    display: flex;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    flex-direction: column;
+    margin-left: 0;
+    margin: auto;
+    font-family: var(--font-familyBody);
+
+    h1 {
+      margin-bottom: 1rem;
+    }
+
+    h2 {
+      font-weight: bold;
+      font-size: 1rem;
+      margin-top: 1.2rem;
+    }
+
+    img {
+      height: 30vh;
+      width: auto;
+      margin-bottom: 5vh;
+    }
+  }
 `;
