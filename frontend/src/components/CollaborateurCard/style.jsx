2f25a8e1ac892a4f7a8bc1fdf9e2b45fae84be2e
Valentin Coppin <valentincoppin@hotmail.fr>
2022-07-01 09:51:30 +0200

    style of collaborateur card

--- frontend/src/components/CollaborateurCard/style.jsx
+++ frontend/src/components/CollaborateurCard/style.jsx
@@ -1,7 +1,7 @@
 import styled from "styled-components";
 
 export default styled.div`
-  height: 70vh;
+  height: 100vh;
   width: 55vw;
   background: var(--colorBackgroundOrangeCard);
   border-radius: 1.5rem;
@@ -12,7 +12,18 @@ export default styled.div`
   flex-direction: column;
   margin-left: 35vw;
 
+  h1 {
+    margin-bottom: 2rem;
+  }
+
   h2 {
     font-weight: bold;
+    font-size: 2rem;
+    margin-top: 1.5rem;
+  }
+
+  img {
+    height: 30vh;
+    width: auto;
   }
 `;
